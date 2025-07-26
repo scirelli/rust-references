0b0b990a6e431dc12e30448b10cd0284e47eac94
Steve Cirelli <scirelli+git@gmail.com>
2025-07-25 20:11:59 -0400

    Learning

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+    let mut s1 = String::from("hello");
+    let len = calcualte_lenth(&s1);
+    let s2 = &mut s1;
+    change(s2);
+    println!("{len} {s2}");
+}
+
+fn calcualte_lenth(s: &String) -> usize {
+    s.len()
+}
+
+fn change(some_string: &mut String) {
+    some_string.push_str(", world!");
 }
